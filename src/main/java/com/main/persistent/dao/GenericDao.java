6a64c98d346a0d6a7276e867f1a8010051a0c4b6
lolik <lolik_@mail.ru>
2016-09-19 08:22:19 +0300

    поправил GenericDao

--- src/main/java/com/main/persistent/dao/GenericDao.java
+++ src/main/java/com/main/persistent/dao/GenericDao.java
@@ -1,20 +1,34 @@
 package com.main.persistent.dao;
 
-import java.io.Serializable;
+import org.hibernate.Session;
 
-public interface GenericDao <T, PK extends Serializable> {
+import java.io.Serializable;
+import java.util.List;
+
+public interface GenericDao <T, I extends Serializable> {
 
     /** Сохранить объект newInstance в базе данных */
-    PK create(T newInstance);
+    I create(T newInstance);
+
+    Session session();
 
     /** Извлечь объект, предварительно сохраненный в базе данных, используя
      *   указанный id в качестве первичного ключа
      */
-    T read(PK id);
+    T getById(String id);
+    T getById(Long id);
+
+    List<T> getAll();
+
+    T persist(T entity);
 
     /** Сохранить изменения, сделанные в объекте.  */
-    void update(T transientObject);
+    void merge(T transientObject);
 
     /** Удалить объект из базы данных */
     void delete(T persistentObject);
+
+    void deleteAll(List<T> entitis);
+
+    long count();
 }
--- src/main/java/com/main/persistent/dao/impl/AbstractGenericDao.java
+++ src/main/java/com/main/persistent/dao/impl/AbstractGenericDao.java
@@ -1,30 +1,74 @@
 package com.main.persistent.dao.impl;
 
 import com.main.persistent.dao.GenericDao;
+import org.hibernate.Session;
 
-public class AbstractGenericDao implements GenericDao<T, PK> { // , FinderExecutor
-    private Class<T> type;
+import javax.persistence.EntityManager;
+import javax.persistence.TypedQuery;
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
+import java.io.Serializable;
+import java.util.List;
 
-    public GenericDaoHibernateImpl(Class<T> type) {
-        this.type = type;
+public abstract class AbstractGenericDao<T, I extends Serializable> implements GenericDao<T, I> { // , FinderExecutor
+
+    private final Class<T> clazz;
+
+    public AbstractGenericDao(Class<T> clazz) {
+        this.clazz = clazz;
     }
 
-    public PK create(T o) {
-        return (PK) getSession().save(o);
+    @Override
+    public Session session() {
+        return getEnttityManager().unwrap(Session.class);
     }
 
-    public T read(PK id) {
-        return (T) getSession().get(type, id);
+    @Override
+    public T getById(String id) {
+        return (T) getEnttityManager().find(clazz, id);
     }
 
-    public void update(T o) {
-        getSession().update(o);
+    @Override
+    public T getById(Long id) {
+        return (T) getEnttityManager().find(clazz, id);
     }
 
-    public void delete(T o) {
-        getSession().delete(o);
+    @Override
+    public List<T> getAll() {
+        CriteriaBuilder cb = getEnttityManager().getCriteriaBuilder();
+        CriteriaQuery<T> cq = cb.createQuery(clazz);
+        TypedQuery<T> query =getEnttityManager().createQuery(cq.select(cq.from(clazz)));
+        return query.getResultList();
     }
 
-    // Не показаны реализации getSession() и setSessionFactory()
+    @Override
+    public T persist(T entity) {
+        getEnttityManager().persist(entity);
+        return entity;
+    }
 
+    @Override
+    public void merge(T entity) {
+        getEnttityManager().merge(entity);
+    }
+
+    @Override
+    public void delete(T entity) {
+        getEnttityManager().remove(entity);
+    }
+
+    @Override
+    public void deleteAll(List<T> entities) {
+        entities.forEach(this::delete);
+    }
+
+    @Override
+    public long count() {
+        CriteriaBuilder cb = getEnttityManager().getCriteriaBuilder();
+        CriteriaQuery<Long> cq = cb.createQuery(Long.class);
+        cq.select(cb.count(cq.from(clazz)));
+        return getEnttityManager().createQuery(cq).getSingleResult();
+    }
+
+    protected abstract EntityManager getEnttityManager();
 }
