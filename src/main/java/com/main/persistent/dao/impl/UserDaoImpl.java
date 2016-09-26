f4c0d626e8f5ff7ddc4d852c5d45805c9716867f
lolik <lolik_@mail.ru>
2016-09-27 00:08:33 +0300

    база данных заработала! jpa select

--- src/main/java/com/main/persistent/dao/impl/UserDaoImpl.java
+++ src/main/java/com/main/persistent/dao/impl/UserDaoImpl.java
@@ -5,6 +5,10 @@ import com.main.persistent.model.User;
 import org.springframework.stereotype.Repository;
 
 import javax.persistence.EntityManager;
+import javax.persistence.TypedQuery;
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Root;
 
 @Repository
 public class UserDaoImpl extends AbstractGenericDao<User, Long> implements UserDao {
@@ -15,14 +19,13 @@ public class UserDaoImpl extends AbstractGenericDao<User, Long> implements UserD
 
     @Override
     public User getByUsername(String username) {
-//        CriteriaBuilder cb = getEntityManager().getCriteriaBuilder();
-//        CriteriaQuery<User> cq = cb.createQuery(User.class);
-//
-//        Root<User> userRoot = cq.from(User.class);
-//        cq.where(cb.equal(userRoot.get(User_.username), username));
-//        TypedQuery<User> query = getEntityManager().createQuery(cq.select(userRoot));
-//        return query.getSingleResult();
-return null ;
+        CriteriaBuilder cb = getEntityManager().getCriteriaBuilder();
+        CriteriaQuery<User> cq = cb.createQuery(User.class);
+
+        Root<User> userRoot = cq.from(User.class);
+        cq.where(cb.equal(userRoot.get(User_.username), username));
+        TypedQuery<User> query = getEntityManager().createQuery(cq.select(userRoot));
+        return query.getSingleResult();
     }
 
 }
