0237ec5041957514759a8645c0a66b7e24876265
Alexei Vilicinschii <Alexei.Vilicinschii@endava.com>
2016-10-05 15:24:42 +0300

    add hibernate-jpamodelgen

--- src/main/java/com/main/persistent/dao/impl/UserDaoImpl.java
+++ src/main/java/com/main/persistent/dao/impl/UserDaoImpl.java
@@ -2,9 +2,9 @@ package com.main.persistent.dao.impl;
 
 import com.main.persistent.dao.UserDao;
 import com.main.persistent.model.User;
+import com.main.persistent.model.User_;
 import org.springframework.stereotype.Repository;
 
-import javax.persistence.EntityManager;
 import javax.persistence.TypedQuery;
 import javax.persistence.criteria.CriteriaBuilder;
 import javax.persistence.criteria.CriteriaQuery;
@@ -23,7 +23,7 @@ public class UserDaoImpl extends AbstractGenericDao<User, Long> implements UserD
         CriteriaQuery<User> cq = cb.createQuery(User.class);
 
         Root<User> userRoot = cq.from(User.class);
-        cq.where(cb.equal(userRoot.get(User_.username), username));
+        cq.where(cb.equal(userRoot.get(User_.userName), username));
         TypedQuery<User> query = getEntityManager().createQuery(cq.select(userRoot));
         return query.getSingleResult();
     }
